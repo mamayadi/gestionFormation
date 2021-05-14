87ef3ab7a41ecd3ed417d785997f2d6618fc2192
mohamed ayadi <mamayadi@gmail.com>
2021-05-14 04:19:57 +0100

    Update groupe

--- src/main/java/spring/jpa/controller/GroupeServiceController.java
+++ src/main/java/spring/jpa/controller/GroupeServiceController.java
@@ -1,46 +1,54 @@
 package spring.jpa.controller;
 
+import java.util.List;
+
+import javax.ws.rs.core.MediaType;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import spring.jpa.model.Groupe;
 import spring.jpa.service.interfaces.GroupeService;
 
 @RestController
+@RequestMapping(path = "/groupes")
 public class GroupeServiceController {
 
 	@Autowired
 	GroupeService groupeService;
-	
-	@RequestMapping(value = "/groupes", method = RequestMethod.GET,produces="application/json")
-	public ResponseEntity<Object> getGroupes() {
-		return new ResponseEntity<>(groupeService.getGroupes(), HttpStatus.OK);
+
+	@GetMapping(value = "/", produces = MediaType.APPLICATION_JSON)
+	public ResponseEntity<List<Groupe>> getGroupes() {
+		return new ResponseEntity<List<Groupe>>(groupeService.getGroupes(), HttpStatus.OK);
 	}
 
-	@RequestMapping(value = "/groupes", method = RequestMethod.POST,produces="application/json")
-	public ResponseEntity<Object> createGroupe(@RequestBody Groupe groupe) {
-		return new ResponseEntity<Object>(groupeService.createGroupe(groupe), HttpStatus.CREATED);
+	@PostMapping(value = "/", consumes = MediaType.APPLICATION_JSON, produces = MediaType.APPLICATION_JSON)
+	public ResponseEntity<Groupe> createGroupe(@RequestBody Groupe groupe) {
+		return new ResponseEntity<Groupe>(groupeService.createGroupe(groupe), HttpStatus.CREATED);
 	}
 
-	@RequestMapping(value = "/groupes/{id}", method = RequestMethod.PUT,produces="application/json")
-	public ResponseEntity<Object> updateGroupe(@PathVariable("id") Long id, @RequestBody Groupe groupe) {
-		return new ResponseEntity<Object>(groupeService.updateGroupe(id, groupe), HttpStatus.OK);
+	@PutMapping(value = "/{id}", consumes = MediaType.APPLICATION_JSON, produces = MediaType.APPLICATION_JSON)
+	public ResponseEntity<Groupe> updateGroupe(@PathVariable("id") Long id, @RequestBody Groupe groupe) {
+		return new ResponseEntity<Groupe>(groupeService.updateGroupe(id, groupe), HttpStatus.OK);
 	}
 
-	@RequestMapping(value = "/groupes/{id}", method = RequestMethod.GET,produces="application/json")
-	public ResponseEntity<Object> getGroupe(@PathVariable("id") Long id) {
-		return new ResponseEntity<>(groupeService.getGroupeById(id), HttpStatus.OK);
+	@GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON)
+	public ResponseEntity<Groupe> getGroupe(@PathVariable("id") Long id) {
+		return new ResponseEntity<Groupe>(groupeService.getGroupeById(id), HttpStatus.OK);
 	}
-	
-	@RequestMapping(value = "/groupes/{id}", method = RequestMethod.DELETE,produces="application/json")
-	public ResponseEntity<Object> deleteGroupe(@PathVariable("id") Long id) {
+
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<?> deleteGroupe(@PathVariable("id") Long id) {
 		groupeService.deleteGroupe(id);
-		return new ResponseEntity<>("Groupe is deleted successsfully", HttpStatus.OK);
+		return new ResponseEntity<>("Groupe is deleted successfully", HttpStatus.OK);
 	}
 }
--- src/main/java/spring/jpa/model/Groupe.java
+++ src/main/java/spring/jpa/model/Groupe.java
@@ -1,5 +1,6 @@
 package spring.jpa.model;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.persistence.Entity;
@@ -8,6 +9,7 @@ import javax.persistence.Id;
 import javax.persistence.ManyToMany;
 import javax.persistence.OneToMany;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 @Entity
@@ -18,12 +20,20 @@ public class Groupe {
 	private Long id;
 	private String libelle;
 	@OneToMany
+	@JsonIgnoreProperties(value = { "groupe", "listNote" })
 	private List<Etudiant> listEtudiant;
 	@ManyToMany
+	@JsonIgnoreProperties(value = { "groupe" })
 	private List<Matiere> listMatiere;
 
 	public Groupe() {
 		// TODO Auto-generated constructor stub
+	}
+
+	public Groupe(String libelle) {
+		this.libelle = libelle;
+		this.listEtudiant = new ArrayList<Etudiant>();
+		this.listMatiere = new ArrayList<Matiere>();
 	}
 
 	public String getLibelle() {
